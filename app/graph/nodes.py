d5339ca34944416cbba98e246f9cfbbf0f00c69e
nithishkumarst17 <nithishkumars222@gmail.com>
2026-08-01 15:38:02 +0530

    added langgraph nodes

--- app/graph/nodes.py
+++ app/graph/nodes.py
@@ -1,53 +1,65 @@
-from typing import Dict
+from app.graph.state import HotelState
+from app.services.embedding_service import EmbeddingService
+from app.services.retriever import RetrieverService
+from app.services.prompt import hotel_prompt
+from app.services.llm_service import SarvamLLM
 
-def classify_question(state) -> Dict:
+
+def classify_question(state: HotelState):
+
     question = state["question"].lower()
+
     if any(word in question for word in ["book", "booking", "reserve"]):
         intent = "booking"
     elif any(word in question for word in ["restaurant", "food", "menu"]):
         intent = "restaurant"
-    else:
+    elif any(word in question for word in ["pool", "wifi", "spa", "room", "check", "hotel"]):
         intent = "hotel_info"
+    else:
+        intent = "general"
     return {
         "intent": intent
     }
 
-def retrieve_documents(state):
-    question = state["question"]
-    docs = [
-        "Hotel has rooftop swimming pool.",
-        "Check-in starts at 2 PM.",
-        "Breakfast is from 7 AM to 10 AM."
-    ]
+def retrieve_documents(state: HotelState):
 
-    context = "\n".join(docs)
+    embedding = EmbeddingService().get_embedding_model()
+
+    retriever = RetrieverService(
+        embedding
+    ).get_retriever()
+
+    docs = retriever.invoke(
+        state["question"]
+    )
+
+    context = "\n\n".join(
+        [
+            doc.page_content
+            for doc in docs
+        ]
+    )
+
     return {
         "retrieved_docs": docs,
         "context": context
     }
 
-def booking_node(state):
+def build_prompt(state: HotelState):
+    prompt = hotel_prompt.format(
+        context=state["context"],
+        question=state["question"]
+    )
+
     return {
-        "answer": "Booking module will be implemented later."
+        "prompt": prompt
     }
 
-def restaurant_node(state):
-    return {
-        "answer": "Restaurant information module will be implemented later."
-    }
-
-def generate_answer(state):
-    prompt = f"""
-You are a Hotel AI Assistant.
-Answer ONLY using the context.
-
-Context:
-{state['context']}
-
-Question:
-{state['question']}
-"""
-    answer = f"Mock Answer:\n{prompt}"
+def generate_answer(state: HotelState):
+    llm = SarvamLLM()
+    answer = llm.invoke(
+        state["prompt"]
+    )
     return {
         "answer": answer
     }
--- app/graph/state.py
+++ app/graph/state.py
@@ -1,7 +1,11 @@
 from typing import TypedDict, List
+from langchain_core.documents import Document
+
+
 class HotelState(TypedDict):
     question: str
     intent: str
+    retrieved_docs: List[Document]
     context: str
+    prompt: str
     answer: str
-    retrieved_docs: List[str]
